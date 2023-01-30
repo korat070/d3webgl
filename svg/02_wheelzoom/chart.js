9159a833d99009f80d6e6265fb49bf513c359181
wnwhale81 <wnwhale81@gmail.com>
2023-01-30 18:05:22 +0900

    콘트롤 줌

---
+++ svg/02_wheelzoom/chart.js
@@ -0,0 +1,335 @@
+const pointColor = ['#ff85ff', '#aa85ff', '#7785ff', ,'#0085ff']
+const margin = { top: 20, right: 15, bottom: 60, left: 70 };
+const outerWidth = 800;
+const outerHeight = 600;
+const width = outerWidth - margin.left - margin.right;
+const height = outerHeight - margin.top - margin.bottom;
+
+export class Chart {
+    constructor(data, id) {
+        this.data = data;
+        this.lastTransform = null;
+        this.lastSelection = null;
+        this.lastXdomain = null;
+        this.lastYdomain = null;
+        this.brushStartPoint = null;
+        this.wrapper = d3.select('#chart-area')
+        this.container = this.wrapper.append("div")
+            .attr('class', 'scatter-container')
+        this.render();
+    }
+
+    render() {
+        this.svgChart = this.container.append('svg:svg')
+            .attr('width', outerWidth)
+            .attr('height', outerHeight)
+            .style('z-index', 1)
+            .attr('class', 'svg-plot')
+            .append('g')
+            .attr('transform', `translate(${margin.left}, ${margin.top})`)
+            .on('dblclick', this.resetZoom.bind(this))
+            .on('click', () => {
+                // console.log(d3.event);
+                // const scaleX = this.lastTransform.rescaleX(this.x);
+                // const scaleY = this.lastTransform.rescaleY(this.y);
+                // console.log(d3.event.x, d3.event.y);
+                // console.log(this.x(d3.event.x), this.y(d3.event.y));
+
+            })
+
+        // this.canvasChart = this.container.append('canvas')
+        //     .attr('width', width)
+        //     .attr('height', height)
+        //     .style('z-index', 2)
+        //     .style('margin-left', margin.left + 'px')
+        //     .style('margin-top', margin.top + 'px')
+        //     .attr('class', 'canvas-plot')
+
+        this.dotSvgChart = this.container.append('svg')
+            .attr('width', outerWidth)
+            .attr('height', outerHeight)
+            .style('z-index', 0)
+            .attr('class', 'svg-plot')
+            .append('g')
+            .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+
+
+        /* this.image = this.container.append('img')
+             .attr('width', width)
+             .attr('height', height)
+             .style('z-index', 0)
+             .style('margin-left', margin.left + 'px')
+             .style('margin-top', margin.top + 'px')
+             .attr('class', 'canvas-plot')
+             .attr('src', './img/mailchimp-0qnRfgnZIsI-unsplash.jpg')*/
+
+
+
+        // this.context = this.canvasChart.node().getContext('2d');
+
+        this.x = d3.scaleLinear().domain([0, d3.max(this.data, (d) => d[0])]).range([0, width]).nice();
+        this.y = d3.scaleLinear().domain([0, d3.max(this.data, (d) => d[1])]).range([height, 0]).nice();
+
+        this.xAxis = d3.axisBottom(this.x);
+        this.yAxis = d3.axisLeft(this.y);
+
+        this.gxAxis = this.svgChart.append('g')
+            .attr('transform', `translate(0, ${height})`)
+            .call(this.xAxis);
+
+        this.gyAxis = this.svgChart.append('g')
+            .call(this.yAxis);
+
+        this.svgChart.append('text')
+            .attr('x', `-${height / 2}`)
+            .attr('dy', '-3.5em')
+            .attr('transform', 'rotate(-90)')
+            .text('Axis Y');
+        this.svgChart.append('text')
+            .attr('x', `${width / 2}`)
+            .attr('y', `${height + 40}`)
+            .text('Axis X');
+
+        this.dot = this.dotSvgChart.append('g')
+            .selectAll('circle')
+            .data(this.data)
+            .enter().append('circle')
+            // .join('circle')
+            .attr('cx', ([x]) => this.x(x))
+            .attr('cy', ([, y]) => this.y(y))
+            .attr('r', 1.5);
+
+        // this.draw(d3.zoomIdentity, 1)
+
+        this.zoom_function = d3.zoom().scaleExtent([1, 1000])
+            .on('zoom', () => {
+                const transform = d3.event.transform;
+
+                this.lastTransform = transform;
+
+                const scaleX = transform.rescaleX(this.x);
+                const scaleY = transform.rescaleY(this.y);
+
+                this.gxAxis.call(this.xAxis.scale(scaleX));
+                this.gyAxis.call(this.yAxis.scale(scaleY));
+                //this.context.clearRect(0, 0, width, height);
+
+                this.lastXdomain = scaleX.domain();
+                this.lastYdomain = scaleY.domain();
+
+                console.log('draw')
+                this.dot.attr('transform', this.lastTransform);
+
+
+            });
+
+        this.dotSvgChart.call(this.zoom_function);
+
+        this.brush = d3.brush().extent([[0, 0], [width, height]])
+            .on("start", () => { this.handleBrushStart(); })
+            .on("brush", () => { this.handleBrush(); })
+            .on("end", () => { this.handleBrushEnd(); })
+            .on("start.nokey", function() {
+                d3.select(window).on("keydown.brush keyup.brush", null);
+            });
+
+        this.brushSvg = this.svgChart
+            .append("g")
+            .attr("class", "brush")
+            .call(this.brush);
+
+        //console.log(this.lastXdomain);
+        //console.log(this.lastYdomain);
+    }
+    draw (transform, groupId) {
+
+
+
+
+
+        //let selectedPoint = getSelectedPoint.call(this, null);
+
+        // console.log("size : ", selectedPoint.length);
+        //console.log("selectedPoint : ", selectedPoint);
+
+
+        // let img = new Image();
+        // img.src = "./img/mailchimp-0qnRfgnZIsI-unsplash.jpg";
+        //
+        // img.onload = () => {
+        //
+        //     const px = scaleX(0);
+        //     const py = scaleY(1000);
+        //
+        //     // console.log(px, py);
+        //     this.context.drawImage(img, px, py, width * transform.k , height * transform.k);
+        //
+        //
+        //     this.data.forEach(point => {
+        //         this.drawPoint(scaleX, scaleY, point, transform.k, groupId);
+        //     });
+        // }
+
+        function getSelectedPoint(groupId) {
+            let selectedPoint = [];
+            if (this.lastTransform.k != 1 && groupId ) {
+                selectedPoint = this.data.filter(point => isBrushed.call(this, point));
+            }
+            return selectedPoint;
+        }
+
+        function isBrushed(point) {
+            if (point[0] >= this.lastXdomain[0]  && point[0] <= this.lastXdomain[1] &&
+                point[1] >= this.lastYdomain[0]  && point[1] <= this.lastYdomain[1]) {
+                return true;
+            }
+            return false;
+        }
+    }
+    drawPoint (scaleX, scaleY, point, k, groupId) {
+        // this.context.beginPath();
+        // this.context.fillStyle = groupId == point[2] ? 'red' : pointColor[Math.floor(Math.random() * 10 % 4)]  ;
+        // const px = scaleX(point[0]);
+        // const py = scaleY(point[1]);
+        //
+        // this.context.arc(px, py, (groupId == point[2] ? 1.2*2 : 1.2) * k, 0, 2 * Math.PI, true);
+        // this.context.fill();
+    }
+    resetZoom () {
+        const t = d3.zoomIdentity.translate(0, 0).scale(1);
+        this.canvasChart
+            .transition()
+            .duration(200)
+            .ease(d3.easeLinear)
+            .call(this.zoom_function.transform, t)
+    }
+    handleBrushStart () {
+        const sourceEvent = d3.event.sourceEvent;
+        const selection = d3.event.selection;
+        // _.log(d3.event)
+
+        if (sourceEvent.type === 'mousedown') {
+            this.brushStartPoint = {
+                mouse: {
+                    x: sourceEvent.screenX,
+                    y: sourceEvent.screenY
+                },
+                x: selection[0][0],
+                y: selection[0][1]
+            }
+            // _.log(this.brushStartPoint)
+        } else {
+            this.brushStartPoint = null;
+        }
+    }
+    handleBrush () {
+        if (this.brushStartPoint !== null) {
+            const scale = width / height;
+            const sourceEvent = d3.event.sourceEvent;
+            const mouse = {
+                x: sourceEvent.screenX,
+                y: sourceEvent.screenY
+            };
+            if (mouse.x < 0) { mouse.x = 0; }
+            if (mouse.y < 0) { mouse.y = 0; }
+            let distance = mouse.y - this.brushStartPoint.mouse.y;
+            let yPosition = this.brushStartPoint.y + distance;
+            let xCorMulti = 1;
+
+            if ((distance < 0 && mouse.x > this.brushStartPoint.mouse.x) || (distance > 0 && mouse.x < this.brushStartPoint.mouse.x)) {
+                xCorMulti = -1;
+            }
+
+            if (yPosition > height) {
+                distance = height - this.brushStartPoint.y;
+                yPosition = height;
+            } else if (yPosition < 0) {
+                distance = -this.brushStartPoint.y;
+                yPosition = 0;
+            }
+
+            let xPosition = this.brushStartPoint.x + distance * scale * xCorMulti;
+            const oldDistance = distance;
+
+            if (xPosition > width) {
+                distance = (width - this.brushStartPoint.x) / scale;
+                xPosition = width;
+            } else if (xPosition < 0) {
+                distance = this.brushStartPoint.x / scale;
+                xPosition = 0;
+            }
+
+            if (oldDistance !== distance) {
+                distance *= (oldDistance < 0) ? -1 : 1;
+                yPosition = this.brushStartPoint.y + distance;
+            }
+
+            const selection = this.svgChart.select(".selection");
+
+            const posValue = Math.abs(distance);
+            selection.attr('width', posValue * scale).attr('height', posValue);
+
+            if (xPosition < this.brushStartPoint.x) {
+                selection.attr('x', xPosition);
+            }
+            if (yPosition < this.brushStartPoint.y) {
+                selection.attr('y', yPosition);
+            }
+
+            const minX = Math.min(this.brushStartPoint.x, xPosition);
+            const maxX = Math.max(this.brushStartPoint.x, xPosition);
+            const minY = Math.min(this.brushStartPoint.y, yPosition);
+            const maxY = Math.max(this.brushStartPoint.y, yPosition);
+
+            this.lastSelection = { x1: minX, x2: maxX, y1: minY, y2: maxY };
+        }
+    }
+    handleBrushEnd () {
+        const s = d3.event.selection;
+
+        console.log("brushCode", d3.brushSelection(this));
+
+        if (!s) {
+            if (this.lastSelection == null) return;
+            // Re-scale axis for the last transformation
+            let zx = this.lastTransform.rescaleX(this.x);
+            let zy = this.lastTransform.rescaleY(this.y);
+
+
+            // Calc distance on Axis-X to use in scale
+            let totalX = Math.abs(this.lastSelection.x2 - this.lastSelection.x1);
+
+            // Get current point [x,y] on canvas
+            const originalPoint = [zx.invert(this.lastSelection.x1), zy.invert(this.lastSelection.y1)];
+            // Calc scale mapping distance AxisX in width * k
+            // Example: Scale 1, width: 830, totalX: 415
+            // Result in a zoom of 2
+            const t = d3.zoomIdentity.scale(((width * this.lastTransform.k) / totalX));
+            // Re-scale axis for the new transformation
+            zx = t.rescaleX(this.x);
+            zy = t.rescaleY(this.y);
+
+
+
+            // Call zoomFunction with a new transformation from the new scale and brush position.
+            // To calculate the brush position we use the originalPoint in the new Axis Scale.
+            // originalPoint it's always positive (because we're sure it's within the canvas).
+            // We need to translate this originalPoint to [0,0]. So, we do (0 - position) or (position * -1)
+            this.canvasChart
+                .transition()
+                .duration(200)
+                .ease(d3.easeLinear)
+                .call(this.zoom_function.transform,
+                    d3.zoomIdentity
+                        .translate(zx(originalPoint[0]) * -1, zy(originalPoint[1]) * -1)
+                        .scale(t.k));
+            // _.log(this.lastSelection)
+            this.lastSelection = null;
+        } else {
+            this.brushSvg.call(this.brush.move, null);
+        }
+    }
+
+}
+
---
+++ svg/02_wheelzoom/main.js
@@ -0,0 +1,42 @@
+import {Chart} from "./chart.js";
+const {L, C} = window._;
+const _ = window._;
+
+let sampleData = [];
+
+
+for (let i = 0; i < 100; i++) {
+    const x = Math.floor(Math.random() * 1000) + 1;
+    const y = Math.floor(Math.random() * 1000) + 1;
+    const groupId = (x % 10) + 1;
+    sampleData.push([x, y, groupId]);
+}
+
+
+async function a() {
+    await _.go(
+        L.interval(0),
+        L.map((a) => a + 1),
+        L.takeUntil((a) => a == 100),
+        L.map( _ => () => new Chart(sampleData)),
+        L.chunk(10),
+        L.map(L.map( f=>f())),
+        _.each(C.takeAll)
+
+    );
+}
+
+a();
+
+
+
+// _.go(
+//     d3.range(0, 1000),
+//     _.map(_ => () => new Chart(sampleData)),
+//     _.chunk(5),
+//     _.each(chunk => {
+//         C.map(f => f(), chunk)
+//     })
+//     // console.log
+//)
+
